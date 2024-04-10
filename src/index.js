db978eed97d318a17036f045846d8bcb23efb994
Ashley Davis <ashley@codecapers.com.au>
2024-04-11 07:32:37 +1000

    Added a message that is set dynamically in the DOM.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import { SplashScreen } from '@capacitor/splash-screen';
+import { SplashScreen } from "@capacitor/splash-screen";
+
+//
+// Replaces the text of a DOM element.
+//
+function replaceText(selector, text) {
+    const element = document.getElementById(selector)
+    if (element) {
+        element.innerText = text;
+    }
+}
 
 window.addEventListener('DOMContentLoaded', () => {
+
+    replaceText('message', "Hello world!");
+
     SplashScreen.hide();
 });
